ed34258863cf6c797d54bc9efe57cdb5e5a85d6c
Akash Kumar <34735962+AllianceSky-PS@users.noreply.github.com>
2018-11-06 08:29:12 +0530

    UD: Fix Crash

--- custom-plugins/urbandictionary.js
+++ custom-plugins/urbandictionary.js
@@ -1,62 +1,70 @@
-"use strict";
+'use strict';
 
-const http = require('http');
+const fs = require('fs');
+const request = require('request');
 
-let udCache = {};
+let urbanCache;
+try {
+	urbanCache = JSON.parse(fs.readFileSync('../config/udcache.json', 'utf8'));
+} catch (e) {
+	urbanCache = {};
+}
+
+function cacheUrbanWord(word, definition) {
+	word = word.toLowerCase().replace(/ /g, '');
+	urbanCache[word] = {"definition": definition, "time": Date.now()};
+	fs.writeFile('config/udcache.json', JSON.stringify(urbanCache));
+}
 
 exports.commands = {
-	"!urbandefine": true,
-	u: "urbandefine",
-	ud: "urbandefine",
-	urbandefine: function (target, room) {
-		if (!this.runBroadcast()) return;
-		if (!target) return this.parse("/help urbandefine");
-		if (target.toString() > 50) return this.errorReply("Phrase can not be longer than 50 characters.");
-
-		if (toId(target) !== "constructor" && udCache[toId(target)]) {
-			this.sendReplyBox(udCache[toId(target)]);
-			if (room) room.update();
-			return;
-		}
+	u: 'ud',
+	urbandefine: 'ud',
+	ud: function (target, room, user, connection, cmd) {
+		if (!target) return this.parse('/help ud');
+		if (target.toString().length > 50) return this.errorReply("Phrase cannot be longer than 50 characters.");
+		if (!this.can('declare', null, room)) return false;
+		if (!this.canTalk()) return this.errorReply("You cannot do this while unable to speak.");
 
 		let options = {
-			host: "api.urbandictionary.com",
-			port: 80,
-			path: `/v0/define?term=${encodeURIComponent(target)}`,
+			url: 'http://www.urbandictionary.com/iphone/search/define',
 			term: target,
+			headers: {
+				'Referer': 'http://m.urbandictionary.com'
+			},
+			qs: {
+				'term': target
+			}
 		};
 
-		http.get(options, res => {
-			let data = ``;
-			res.on(`data`, chunk => {
-				data += chunk;
-			}).on(`end`, () => {
-				if (data.charAt(0) !== `{`) {
-					this.sendReplyBox(`Error retrieving definition for <strong>"${target}"</strong>.`);
-					if (room) room.update();
-					return;
-				}
-				data = JSON.parse(data);
-				let definitions = data[`list`];
-				if (data[`result_type`] === `no_results` || !data) {
-					this.sendReplyBox(`No results for <strong>"${target}"</strong>.`);
-					if (room) room.update();
-					return;
+		let milliseconds = ((44640 * 60) * 1000);
+
+		if (urbanCache[target.toLowerCase().replace(/ /g, '')] && Math.round(Math.abs((urbanCache[target.toLowerCase().replace(/ /g, '')].time - Date.now()) / (24 * 60 * 60 * 1000))) < 31) {
+			return this.sendReplyBox("<b>" + Chat.escapeHTML(target) + ":</b> " + urbanCache[target.toLowerCase().replace(/ /g, '')].definition.substr(0, 400));
+		}
+
+		let self = this;
+
+		function callback(error, response, body) {
+			if (!error && response.statusCode === 200) {
+				let page = JSON.parse(body);
+				let definitions = page['list'];
+				if (page['result_type'] === 'no_results') {
+					self.sendReplyBox("No results for <b>\"" + Chat.escapeHTML(target) + "\"</b>.");
+					return room.update();
 				} else {
-					if (!definitions[0][`word`] || !definitions[0][`definition`]) {
-						this.sendReplyBox(`No results for <strong>"${target}"</strong>.`);
-						if (room) room.update();
-						return;
+					if (!definitions[0]['word'] || !definitions[0]['definition']) {
+						self.sendReplyBox("No results for <b>\"" + Chat.escapeHTML(target) + "\"</b>.");
+						return room.update();
 					}
-					let output = `<strong>${definitions[0][`word`]}</strong> ${definitions[0][`definition`].replace(/\r\n/g, `<br />`).replace(/\n/g, ` `)}`;
-					if (output.length > 400) output = output.slice(0, 400) + `...`;
-					this.sendReplyBox(output);
-					udCache[toId(target)] = output;
-					if (room) room.update();
-					return;
+					let output = "<b>" + Chat.escapeHTML(definitions[0]['word']) + ":</b> " + Chat.escapeHTML(definitions[0]['definition']).replace(/\r\n/g, '<br />').replace(/\n/g, ' ');
+					if (output.length > 400) output = output.slice(0, 400) + '...';
+					cacheUrbanWord(target, Chat.escapeHTML(definitions[0]['definition']).replace(/\r\n/g, '<br />').replace(/\n/g, ' '));
+					self.sendReplyBox(output);
+					return room.update();
 				}
-			});
-		});
+			}
+		}
+		request(options, callback);
 	},
-	urbandefinehelp: ["/u [word] - Gives the Urban Definition for a word."],
+	udhelp: ["/urbandefine [phrase] - Shows the urban definition of the phrase. If you don't put in a phrase, it will show you a random phrase from urbandefine."]
 };
