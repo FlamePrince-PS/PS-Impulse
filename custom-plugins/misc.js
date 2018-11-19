6a246de94f7a5979a0f1ef1829a4e7ecb2483ea6
Musaddik Temkar <44061946+FlamePrince-PS@users.noreply.github.com>
2018-11-19 12:52:29 +0530

    Misc: Add Poof.

--- custom-plugins/misc.js
+++ custom-plugins/misc.js
@@ -24,6 +24,38 @@ const asciiMap = new Map([
 	["\u2460", "1"], ["\u2461", "2"], ["\u2462", "3"], ["\u2463", "4"], ["\u2464", "5"], ["\u2465", "6"], ["\u2466", "7"], ["\u2467", "8"], ["\u2468", "9"], ["\u24EA", "0"],
 ]);
 
+const messages = [
+	"ventured into Shrek's Swamp.",
+	"disrespected the OgreLord!",
+	"used Explosion!",
+	"was swallowed up by the Earth!",
+	"was eaten by Lex!",
+	"was sucker punched by Absol!",
+	"has left the building.",
+	"got lost in the woods!",
+	"left for their lover!",
+	"was hit by Magikarp's Revenge!",
+	"was sucked into a whirlpool!",
+	"got scared and left the server!",
+	"went into a cave without a repel!",
+	"got eaten by a bunch of piranhas!",
+	"ventured too deep into the forest without an escape rope",
+	"got shrekt",
+	"woke up an angry Snorlax!",
+	"was forced to give jd an oil massage!",
+	"was used as shark bait!",
+	"peered through the hole on Shedinja's back",
+	"received judgment from the almighty Arceus!",
+	"used Final Gambit and missed!",
+	"went into grass without any Pokemon!",
+	"made a Slowbro angry!",
+	"took a focus punch from Breloom!",
+	"got lost in the illusion of reality.",
+	"ate a bomb!",
+	"left for a timeout!",
+	"fell into a snake pit!",
+];
+
 function parseStatus(text, encoding) {
 	if (encoding) {
 		text = text
@@ -146,6 +178,40 @@ exports.commands = {
 	},
 	backhelp: ["/back - Sets a users away status back to normal."],
 
+	d: 'poof',
+	cpoof: 'poof',
+	poof: function (target, room, user) {
+		if (Config.poofOff) return this.sendReply("Poof is currently disabled.");
+		if (target && !this.can('broadcast')) return false;
+		/*if (room.id !== 'lobby') return false;*/
+		let message = target || messages[Math.floor(Math.random() * messages.length)];
+		if (message.indexOf('{{user}}') < 0) message = '{{user}} ' + message;
+		message = message.replace(/{{user}}/g, user.name);
+		if (!this.canTalk(message)) return false;
+
+		let colour = '#' + [1, 1, 1].map(function () {
+			let part = Math.floor(Math.random() * 0xaa);
+			return (part < 0x10 ? '0' : '') + part.toString(16);
+		}).join('');
+
+		room.addRaw('<center><strong><font color="' + colour + '">~~ ' + Chat.escapeHTML(message) + ' ~~</font></strong></center>');
+		user.lastPoof = Date.now();
+		user.lastPoofMessage = message;
+		user.disconnectAll();
+	},
+
+	poofoff: 'nopoof',
+	nopoof: function () {
+		if (!this.can('ban')) return false;
+		Config.poof = true;
+		return this.sendReply("Poof is now disabled.");
+	},
+
+	poofon: function () {
+		if (!this.can('ban')) return false;
+		Config.poof = false;
+		return this.sendReply("Poof is now enabled.");
+	},
 
 	contact: 'whotocontact',
 	wtc: 'whotocontact',
